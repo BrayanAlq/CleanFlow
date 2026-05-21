889368d109721e51e58f87909979b3b94a83797d
deyvigo <deyvigo23@gmail.com>
2026-05-21 00:33:01 -0500

    fix: endpoint to set actual container (id validation)

--- cleanflowback/src/main/java/com/example/cleanflowback/service/implement/ActualContainerServiceImpl.java
+++ cleanflowback/src/main/java/com/example/cleanflowback/service/implement/ActualContainerServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.cleanflowback.service.implement;
 
 import com.example.cleanflowback.dto.in.ActualContainerRequestDTO;
+import com.example.cleanflowback.exception.ResourceNotFoundException;
 import com.example.cleanflowback.model.ActualContainerEntity;
+import com.example.cleanflowback.model.ContainerEntity;
 import com.example.cleanflowback.repository.ActualContainerRepository;
+import com.example.cleanflowback.repository.ContainerRepository;
 import com.example.cleanflowback.service.ActualContainerService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,10 +16,13 @@ import java.util.List;
 @AllArgsConstructor
 public class ActualContainerServiceImpl implements ActualContainerService {
     private final ActualContainerRepository actualContainerRepository;
+    private final ContainerRepository containerRepository;
 
     @Override
     public void createOrUpdateActualContainer(ActualContainerRequestDTO requestDTO) {
         List<ActualContainerEntity> actualContainerEntities = actualContainerRepository.findAll();
+        ContainerEntity container = containerRepository.findById(requestDTO.containerId())
+            .orElseThrow(() -> new ResourceNotFoundException("Container not found"));
 
         ActualContainerEntity newActualContainerEntity = new ActualContainerEntity();
 
