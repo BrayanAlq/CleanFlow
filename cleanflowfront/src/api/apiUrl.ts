a1b6970d6a270aab8843991dce2617d36587754a
deyvigo <deyvigo23@gmail.com>
2026-05-25 18:59:31 -0500

    fix: a

--- cleanflowfront/src/api/apiUrl.ts
+++ cleanflowfront/src/api/apiUrl.ts
@@ -1,4 +1,1 @@
-export const API_URL = 'http://localhost:8080'
-export const JWT_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFkbWluMSIsInJvbGUiOiJBRE1JTiIsImlkIjoxLCJmaXJzdF9uYW1lIjoiQWRtaW4iLCJsYXN0X25hbWUiOiJMaW1hIiwiZXhwIjoxNzc4ODgxMzE3fQ.mNn1pSVHwcsyXy2GTXCf06ajb1oNcxwEcY5ipqmLPvw'
-
-export const WS_URL = 'ws://localhost:8080/ws'
+export const API_URL = import.meta.env.VITE_API_BASE_URL
