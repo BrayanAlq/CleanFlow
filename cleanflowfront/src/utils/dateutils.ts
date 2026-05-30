23261f5b0d98d5ea742e84ff2eca3c215c9562c8
deyvigo <deyvigo23@gmail.com>
2026-05-30 14:06:37 -0500

    fix: date time

--- cleanflowfront/src/utils/dateutils.ts
+++ cleanflowfront/src/utils/dateutils.ts
@@ -1,15 +1,20 @@
 import dayjs from "dayjs"
 import "dayjs/locale/es"
+import utc from "dayjs/plugin/utc"
+import timezone from "dayjs/plugin/timezone"
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 dayjs.locale("es")
 
 export const formatDate = (dateString: string) => {
-  return dayjs(dateString).format("D [de] MMMM YYYY, HH:mm")
+  return dayjs(dateString).local().format("D [de] MMMM YYYY, HH:mm")
 }
 
 export const formatToDate = (dateString: string) => {
-  return dayjs(dateString).format("D [de] MMMM YYYY")
+  return dayjs(dateString).local().format("D [de] MMMM YYYY")
 }
 
 export const formatToHour = (dateString: string) => {
-  return dayjs(dateString).format("HH:mm")
+  return dayjs(dateString).local().format("HH:mm")
 }
