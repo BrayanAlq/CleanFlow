d15d66592c03c97e563f89d0df29822fd11a3ab7
deyvigo <deyvigo23@gmail.com>
2026-05-30 14:35:37 -0500

    fix: stomp provider position

--- cleanflowfront/src/components/Layout.tsx
+++ cleanflowfront/src/components/Layout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom"
 import { NavBar } from "@/components/NavBar"
+import { StompProvider } from "@/context/StompContext"
 
 export const Layout = () => {
   return (
-    <div className="flex w-dvw h-dvh bg-black">
-      <NavBar />
-      <div className="flex flex-1 items-center justify-center">
-        <Outlet />
+    <StompProvider>
+      <div className="flex w-dvw h-dvh bg-black">
+        <NavBar />
+        <div className="flex flex-1 items-center justify-center">
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </StompProvider>
   )
 }
--- cleanflowfront/src/main.tsx
+++ cleanflowfront/src/main.tsx
@@ -1,6 +1,5 @@
 import App from '@/App.tsx'
 import { AuthProvider } from '@/auth/AuthContext.tsx'
-import { StompProvider } from '@/context/StompContext.tsx'
 import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -12,12 +11,10 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <StompProvider>
-        <QueryClientProvider client={queryClient}>
-          <Toaster position="bottom-center" />
-          <App />
-        </QueryClientProvider>
-      </StompProvider>
+      <QueryClientProvider client={queryClient}>
+        <Toaster position="bottom-center" />
+        <App />
+      </QueryClientProvider>
     </AuthProvider>
   </StrictMode>,
 )
